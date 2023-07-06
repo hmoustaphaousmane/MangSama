9809081210b79264bc14ca9c9e38af1972701771
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2023-07-06 17:00:11 +0100

    (feat) : set users authentication with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
-    has_many :appreciations
-    has_many :mangas, through: :appreciations
-    has_many :favorites
-    has_many :mangas, through: :favorites
-    has_many :comments
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :appreciations
+  has_many :mangas, through: :appreciations
+  has_many :favorites
+  has_many :mangas, through: :favorites
+  has_many :comments
 end 
