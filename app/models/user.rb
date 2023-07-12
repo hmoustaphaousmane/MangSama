dbbe1de04ac6355f27bfca9c1654c7b1700db266
abdoulaye <abdoulaye@abdoulaye.com>
2023-07-12 20:26:53 +0000

    fix model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,27 @@ class User < ApplicationRecord
   has_many :comments
 
   has_one_attached :profile_photo
+
+  def like(manga)
+    appreciation = Appreciation.find_or_initialize_by(user_id: id, manga_id: manga.id)
+    appreciation.liked = true
+    appreciation.save
+  end
+  
+  def dislike(manga)
+    appreciation = Appreciation.find_or_initialize_by(user_id: id, manga_id: manga.id)
+    appreciation.liked = false
+    appreciation.save
+  end
+  
+  # Methode qui permet de se connecter avec le username
+  def self.find_first_by_auth_conditions(warden_conditions) # warden_conditions est l'outil d'authentification utilisé derière devise
+    conditions = warden_conditions.dup    
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where("lower(username) = :value OR lower(email) = :value", value: login.downcase).first
+    else
+      where(conditions.to_hash)
+    end
+      
+  end
 end
